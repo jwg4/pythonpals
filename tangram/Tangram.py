01be73078b69d8b2ca356cbdd3c7a034dfb2771f
Jack Grahl <jack.grahl@gmail.com>
2017-05-29 10:14:00 +0000

    Remove left over stuff from jupyter.

--- tangram/Tangram.py
+++ tangram/Tangram.py
@@ -1,8 +1,3 @@
-
-# coding: utf-8
-
-# In[5]:
-
 shapes = {
     'square': [(0,0), (1,1), (2,2), (1,-1)],
     'medium triangle': [(0,0), (2,2), (2,0)],
@@ -21,13 +16,7 @@ piece_names = [
     'big triangle'
 ]
 
-
-# In[6]:
-
 [ shapes[name] for name in piece_names ]
-
-
-# In[7]:
 
 def rotate(shape, angle):
     ...
@@ -38,10 +27,5 @@ def translate(shape, x, y):
     [(1, 7), (3, 9), (5, 7)]
     """
     ...
-
-
-# In[ ]:
-
-# Encode a simple problem
 
 
