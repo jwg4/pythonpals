c465628e32209497940e2b0aeb79fd2b91f76276
Jack Grahl <jack.grahl@gmail.com>
2017-09-05 21:28:33 +0000

    More whitespace fixes.
    These are purely a matter of stlye and do not affect the code or behavior.

--- tangram/Tangram.py
+++ tangram/Tangram.py
@@ -1,32 +1,31 @@
 # coding: utf-8
-
-
 import numpy as np
 import math
 
+
 # Define square, corners, string method
 class Square(object):
-    
+
     def __init__(self, points):
         self.points = points
-    
+
     def __str__(self):
         return "%s %s %s %s" % (self.points[0], self.points[1], self.points[2], self.points[3])
-            
+
 
 # Define triangle, corners, string method
 class Triangle(object):
-    
+
     def __init__(self, points):
         self.points = points
-        
+
     def __str__(self):
         return "%s %s %s" % (self.points[0], self.points[1], self.points[2])
-    
+
 
 # Define parallelogram, corners, string method
 class Parallel(object):
-    
+
     def __init__(self, points):
         self.points = points
 
@@ -37,28 +36,28 @@ class Parallel(object):
 # Translates shape by adding translation vector to coordinate vectors in turn
 # Usage: 'shape' = shape to be translated, 'x' = units to move in x direction, 'y' = units to move in y direction
 def trans(shape, x, y):
-    
+
     trans_vec = np.array([x, y])
-    
+
     for i in range(len(shape.points)):
         shape.points[i] = shape.points[i] + trans_vec
-    
+
 
 # Function 'reflect'
 # Reflects shape across x or y axis
 # Usage: 'shape' = shape to be reflected
 # Usage: set 'x' = -1 to reflect across x-axis, else set as 1, same applies to 'y'
 def reflect(shape, x, y):
-    
+
     # As reflecting about origin, determine whether point1 is at origin
     # If not, save temp coordinates and move point1 to origin
     at_origin = np.array_equal(shape.points[0], np.array([0, 0]))
-    
+
     if at_origin == False:
         temp_point = shape.points[0]
-        
+
         trans(shape, -temp_point[0], -temp_point[1])
-    
+
     # Reflect across x-axis then y-axis as appropriate
     for i in range(len(shape.points)):
         shape.points[i][0] = shape.points[i][0] * y
